6e3b3b5ee5ef1c0e57235f8f0b9b36719ec273d7
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-26 18:10:07 +0100

    scoreboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer()
 
+scoreboard = Scoreboard()
 snake = Snake()
 food = Food()
 
@@ -29,6 +31,6 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
-        
+
 
 screen.exitonclick()
